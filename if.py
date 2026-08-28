243be5d1af4003e5a4f1bd939e2d41b369c73f64
barbod <barbod@fgdhgmail.com>
2026-08-28 14:09:32 +0800

    barbod

---
+++ if.py
@@ -0,0 +1,9 @@
+name = input("enter you are name: ")
+age =input("enter you are age: ")
+if name == "barbod":
+    print("hello barbod")
+
+elif age == 13:
+    print("you are 13 years old")
+else:
+    print("i do not know you")   
